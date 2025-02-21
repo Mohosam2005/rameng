b6ee699e37205ef9ed92d2f4fbe378dea3c7423c
Mohosam2005 <mohosam2006@gmail.com>
2025-02-22 00:34:49 +0200

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,9 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
-// تكوين Supabase
-const supabaseUrl = 'https://omxetmxlrxsmizgxhyyb.supabase.co'; // استبدل بـ URL الخاص بك
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9teGV0bXhscnhzbWl6Z3hoeXliIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDAxNzA2MTYsImV4cCI6MjA1NTc0NjYxNn0.DDkGivdnZV02yzxbyO9Z0Xn2aSlcRXSIgjTnV7nIoro'; // استبدل بـ anon key الخاص بك
+const supabaseUrl = 'https://omxetmxlrxsmizgxhyyb.supabase.co';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9teGV0bXhscnhzbWl6Z3hoeXliIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDAxNzA2MTYsImV4cCI6MjA1NTc0NjYxNn0.DDkGivdnZV02yzxbyO9Z0Xn2aSlcRXSIgjTnV7nIoro'; // تأكد من أن هذا المفتاح صحيح وكامل
 const supabase = createClient(supabaseUrl, supabaseKey);
 
-// تصدير supabase للاستخدام في المكونات الأخرى
 export { supabase };
