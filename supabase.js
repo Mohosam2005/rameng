cc3c87019a99e334b2c9e02702f0bbaf063cc986
Mohosam2005 <mohosam2006@gmail.com>
2025-02-21 23:49:52 +0200

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,12 +1,9 @@
 import { createClient } from '@supabase/supabase-js';
 
 // تكوين Supabase
-
-import { createClient } from '@supabase/supabase-js'
-
-const supabaseUrl = 'https://omxetmxlrxsmizgxhyyb.supabase.co'
-const supabaseKey = process.env.SUPABASE_KEY
-const supabase = createClient(supabaseUrl, supabaseKey)
+const supabaseUrl = 'https://omxetmxlrxsmizgxhyyb.supabase.co'; // استبدل بـ URL الخاص بك
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9teGV0bXhscnhzbWl6Z3hoeXliIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDAxNzA2MTYsImV4cCI6MjA1NTc0NjYxNn0.DDkGivdnZV02yzxbyO9Z0Xn2aSlcRXSIgjTnV7nIoro'; // استبدل بـ anon key الخاص بك
+const supabase = createClient(supabaseUrl, supabaseKey);
 
 // تصدير supabase للاستخدام في المكونات الأخرى
 export { supabase };
